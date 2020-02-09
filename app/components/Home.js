b2b0f804efe998a5318d5119fbd8ba313645ff59
Rémi Deprez <grolent@gmail.com>
2020-02-09 14:22:09 +0100

    test

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,35 @@ import DeckSummary from "./DeckSummary";
 import { Card } from "antd";
 
 const Home = () => {
-  const gameResults = useGetGamesResults();
+  const gameResults = [
+    {
+      localPlayerWin: true,
+      deck: {
+        DeckCode: "ADECKCODE",
+        CardsInDeck: {
+          "01SI004": 3,
+          "01SI021": 3,
+          "01IO003": 3,
+          "01IO026": 3,
+          "01IO049": 3,
+          "01IO009": 2,
+          "01IO044": 2,
+          "01IO057": 2,
+          "01SI022": 2,
+          "01SI023": 2,
+          "01SI034": 2,
+          "01SI035": 2,
+          "01SI042": 2,
+          "01SI043": 2,
+          "01SI048": 2,
+          "01SI049": 2,
+          "01SI009": 1,
+          "01SI033": 1,
+          "01SI040": 1
+        }
+      }
+    }
+  ];
 
   return (
     <div className="App">
