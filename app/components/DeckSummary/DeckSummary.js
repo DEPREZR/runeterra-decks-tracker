6318c378f83560380459746553cdd85c85148ed8
Rémi Deprez <grolent@gmail.com>
2020-02-20 15:37:43 +0100

    Release bump data persist test

--- app/components/DeckSummary/DeckSummary.js
+++ app/components/DeckSummary/DeckSummary.js
@@ -33,6 +33,7 @@ const DeckSummary = ({ gameResult }) => {
         style={{ color: "rgb(167,167,199)" }}
       >
         <span className="ml-3">Victoires : {gameResult.winAmount}</span>
+        <span>/</span>
         <span className="mr-3">Défaites : {gameResult.defeatAmount}</span>
       </div>
     </div>
