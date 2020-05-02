c704c2b8ce48def569d7824945967f5c6ab6be4c
Rémi Deprez <grolent@gmail.com>
2020-05-02 18:19:48 +0200

    fix using of dataset 2

--- app/cardsDataHelpers/cardsDataHelpers.js
+++ app/cardsDataHelpers/cardsDataHelpers.js
@@ -1,5 +1,5 @@
 const cardsDataSet1 = require("../assets/datadragon-set1-lite-fr_fr/fr_fr/data/set1-fr_fr.json");
-const cardsDataSet2 = require("../assets/datadragon-set1-lite-fr_fr/fr_fr/data/set2-fr_fr.json");
+const cardsDataSet2 = require("../assets/datadragon-set2-lite-fr_fr/fr_fr/data/set2-fr_fr.json");
 
 const cardsData = [...cardsDataSet1, ...cardsDataSet2];
 
